22e88045d1c13362758c789abfa5343f61b65819
Lucas Novaes <lucasnovaesprog@hotmail.com>
2023-08-22 09:20:09 -0300

    feat: :sparkles: generating random users

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const doubleBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await rest.json();
+
+  console.log(data);
+}
